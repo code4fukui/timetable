6287d0d7229f85bbdcc078ce3bcbea9b4f3dce82
Taisuke Fukuno <fukuno@jig.jp>
2020-04-18 19:48:38 +0900

    一部データの項目に長さ追加

--- node/youtubeplaylist.mjs
+++ node/youtubeplaylist.mjs
@@ -5,9 +5,67 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const fetchYouTubePlaylist = async function(playlistid) {
-  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistid}&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`
+  const part = 'snippet'
+  //const part = 'contentDetails'
+  //const part = 'status'
+  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=${part}&playlistId=${playlistid}&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`
   const json = await (await fetch(url)).json()
   return json
+}
+const fetchYouTubeChannel = async function(id) {
+  //const part = 'snippet'
+  const part = 'contentDetails'
+  //const part = 'status'
+  const url = `https://www.googleapis.com/youtube/v3/channels?part=${part}&id=${id}&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`
+  const json = await (await fetch(url)).json()
+  return json
+}
+const fetchYouTubeVideo = async function(videoid) {
+  if (videoid.startsWith('https://')) {
+    const ss = videoid.match(/https:\/\/www.youtube.com\/watch\?v=([^\&]+)/)
+    if (!ss)
+      return null
+    videoid = ss[1]
+  }
+  const part = 'contentDetails'
+  const url = `https://www.googleapis.com/youtube/v3/videos?part=${part}&id=${videoid}&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`
+  const json = await (await fetch(url)).json()
+  return json
+}
+const parseDuration = function(duration) {
+  const fix0 = util.fix0
+  let num = duration.match(/PT(\d+)S/) // PT27S
+  if (num) {
+    return '00:00:' + fix0(num[1], 2)
+  }
+  num = duration.match(/PT(\d+)M(\d+)S/) // PT12M27S
+  if (num) {
+    return '00:' + fix0(num[1], 2) + ':' + fix0(num[2], 2)
+  }
+  num = duration.match(/PT(\d+)H(\d+)M(\d+)S/) // PT1H12M27S
+  if (num) {
+    return fix0(num[1], 2) + ':' + fix0(num[2], 2) + ':' + fix0(num[3], 2)
+  }
+  console.log('parse err', duration)
+  return null
+}
+const fetchYouTubeVideoDuration = async function(videoid) {
+  if (Array.isArray(videoid)) {
+    const json = await fetchYouTubeVideo(videoid.join(','))
+    if (!json)
+      return null
+    const res = []
+    for (const d of json.items) {
+      res.push(parseDuration(d.contentDetails.duration))
+    }
+    return res
+  } else {
+    const json = await fetchYouTubeVideo(videoid)
+    if (!json)
+      return null
+    const d = json.items[0].contentDetails
+    return parseDuration(d.duration)
+  }
 }
 
 const normalizeTarget = function(a) {
@@ -24,9 +82,9 @@ const normalizeTarget = function(a) {
     return '中学' + a.charAt(1) + '年'
   } else if (a.match(/高\d/)) {
     return '高校' + a.charAt(1) + '年'
-  } else if (a.match(/小学校\d年生/)) {
+  } else if (a.match(/小学校\d年/)) {
     return '小学' + a.charAt(3) + '年'
-  } else if (a.match(/中学校\d年生/)) {
+  } else if (a.match(/中学校\d年/)) {
     return '中学' + a.charAt(3) + '年'
   } else if (a.match(/高校\d年生/)) {
     return '高校' + a.charAt(3) + '年'
@@ -41,46 +99,124 @@ const normalizeType = function(s) {
     return '体育'
   } else if (s == '図画工作') {
     return '図工'
+  } else if (s == '外国語活動') {
+    return '外国語'
   }
   return s
 }
 
-const main = async function() {
-  const playlistid = 'PLhOpFff6DKIkPLwurIS8cnVUw6-_FkXXj'
-  const data = await fetchYouTubePlaylist(playlistid)
-  const path = '../data/saitamacity/'
-  //console.log(data)
+const filterSaitama = function(stitle) {
+  let target, type, title
+  if (stitle.startsWith('特別支援')) {
+    const ss = util.splitString(stitle, ' 　「」')
+    target = '特別支援'
+    type = ''
+    title = ss.slice(1).join(' ')
+  } else {
+    const ss = util.splitString(stitle, ' 　「」')
+    target = ss[0]
+    type = ss[1]
+    title = ss.slice(2).join(' ')
+  }
+  return {
+    '対象': normalizeTarget(target),
+    '科目': normalizeType(type),
+    'タイトル': title,
+  }
+}
+const normalizeTitle = function(t) {
+  const cnum = '①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⑯⑰⑱⑲⑳㉑㉒㉓㉔㉕㉖㉗㉘㉙㉚㉛㉜㉝㉞㉟㊱㊲㊳㊴㊵㊶㊷㊸㊹㊺㊻㊼㊽㊾㊿'
+  for (let i = 0; i < cnum.length; i++) {
+    t = t.replace(cnum.charAt(i), '(' + (i + 1) + ')')
+  }
+  return util.toHalf(t)
+}
+// 【ふくいわくわく授業】小学校1年国語①(えんぴつとなかよくなろう)
+const filterFukui = function(stitle) {
+  const s = stitle.substring('【ふくいわくわく授業】'.length)
+  const ss = util.splitString(s, '年（）')
+  const target = ss[0] + "年"
+  let type = normalizeTitle(ss[1])
+  let n = type.indexOf('(')
+  let no = ""
+  if (n >= 0) {
+    const num = type.match(/(.+)\((\d+)\)/)
+    type = num[1]
+    no = num[2]
+  }
+  const title = normalizeTitle(ss[2])
+  return {
+    '対象': normalizeTarget(target),
+    '科目': normalizeType(type),
+    'No': no,
+    'タイトル': title,
+  }
+}
+
+const makeCSV = async function(type, listid, name, filter) {
+  const path = '../data/' + name + "/"
+  let data = null
+  if (type == 'playlist') {
+    data = await fetchYouTubePlaylist(listid)
+  } else if (type == 'channel') {
+    data = await fetchYouTubeChannel(listid)
+    const pid = data.items[0].contentDetails.relatedPlaylists.uploads // UU_ZMXFvvu-YWEbk0wK79jhw
+    data = await fetchYouTubePlaylist(pid)
+    /*
+    console.log(data)
+    console.log(data.items[0].contentDetails)
+    return
+    */
+  } else {
+    console.log('ignore type')
+    return null
+  }
+
   const list = []
+  const videoids = []
   for (const d of data.items) {
     const s = d.snippet
     if (!s.thumbnails) // private video
       continue
     console.log(s)
-    let target, type, title
-    if (s.title.startsWith('特別支援')) {
-      const ss = util.splitString(s.title, ' 　「」')
-      target = '特別支援'
-      type = ''
-      title = ss.slice(1).join(' ')
-    } else {
-      const ss = util.splitString(s.title, ' 　「」')
-      target = ss[0]
-      type = ss[1]
-      title = ss.slice(2).join(' ')
-    }
-    list.push({
-      '公開日': s.publishedAt,
-      'URL': 'https://www.youtube.com/watch?v=' + s.resourceId.videoId,
-      '対象': normalizeTarget(target),
-      '科目': normalizeType(type),
-      'タイトル': title,
-    })
+    const o = filter(s.title)
+    const videoid = s.resourceId.videoId
+    o['公開日'] = s.publishedAt
+    o.URL = 'https://www.youtube.com/watch?v=' + videoid
+    list.push(o)
+    videoids.push(videoid)
+  }
+  const duration = await fetchYouTubeVideoDuration(videoids)
+  for (let i = 0; i < list.length; i++) {
+    list[i]['長さ'] = duration[i]
   }
   const dt = util.formatYMD()
   //const fnindex = 'index.csv'
   console.log(list)
-  fs.writeFileSync(path + dt + '.csv', util.addBOM(util.encodeCSV(util.json2csv(list))), 'utf-8')
-  fs.writeFileSync(path + 'index.csv', util.addBOM(util.encodeCSV(util.json2csv(list))), 'utf-8')
+  const scsv = util.addBOM(util.encodeCSV(util.json2csv(list)))
+  fs.writeFileSync(path + dt + '.csv', scsv, 'utf-8')
+  fs.writeFileSync(path + 'index.csv', scsv, 'utf-8')
+}
+
+const main = async function() {
+  
+  //const videoid = 'H_aZRDq9Qxg'
+  //const videoid = 'https://www.youtube.com/watch?v=_yncog6eojs&t=16s'
+  //const videoid = 'dtauGqnQpig'
+  /*
+  const videoid = 'H_aZRDq9Qxg,dtauGqnQpig'
+  const v = await fetchYouTubeVideoDuration(videoid)
+  console.log(v) // duration: 'PT12M27S',
+  console.log(await fetchYouTubeVideoDuration([ 'H_aZRDq9Qxg', 'dtauGqnQpig' ]))
+  return
+  */
+  const contents = [
+    { name: 'fukuipref', type: 'channel', id: 'UC_ZMXFvvu-YWEbk0wK79jhw', filter: filterFukui },
+    //{ name: 'saitamacity', type: 'playlist', id: 'PLhOpFff6DKIkPLwurIS8cnVUw6-_FkXXj', filter: filterSaitama },
+  ]
+  for (const c of contents) {
+    await makeCSV(c.type, c.id, c.name, c.filter)
+  }
 }
 if (process.argv[1].endsWith('/youtubeplaylist.mjs')) {
   main()
