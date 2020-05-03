3702109c00449e7240f79a319db7a58deb91c761
Taisuke Fukuno <fukuno@jig.jp>
2020-05-03 20:37:04 +0900

    データ更新

--- node/makedata.mjs
+++ node/makedata.mjs
@@ -1,6 +1,6 @@
 import util from './util.mjs'
 
-const makeData = async function() {
+const makeData = async function () {
   const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQdsFYLU6roC8Y-PB-7MSJxxVByBh-fQxpa-Bcf0Hatykrv8M20ZQ0NAwFF5MBjw7qTZ2NnKRNEBNeP/pub?gid=0&single=true&output=csv'
   const path = '../data/'
   const baseurl = 'https://code4fukui.github.io/timetable/data/'
@@ -21,19 +21,19 @@ const makeData = async function() {
         util.writeCSV(path + fn, util.json2csv(items))
         item.URL = baseurl + fn + '.csv'
       }
-      //const d = util.setJSON({}, item)
+      // const d = util.setJSON({}, item)
       let cnt = 0
       for (const a of items) {
-        if (a['有効'] && parseInt(a['有効']) == 0) {
+        if (a['有効'] && parseInt(a['有効']) === 0) {
           continue
         }
         const d2 = util.copyJSON(item)
         util.setJSON(d2, a)
         delete d2['備考']
         delete d2['有効']
+        if (!d2['タイトル']) { d2['タイトル'] = d2['教材シリーズ名'] }
         data.push(d2)
-        if (cnt++ < 3)
-          console.log(d2)
+        if (cnt++ < 3) { console.log(d2) }
       }
     } else {
       delete item['備考']
@@ -44,7 +44,7 @@ const makeData = async function() {
   console.log(data.length)
   util.writeCSV(path + fn, util.json2csv(data))
 }
-const main = async function() {
+const main = async function () {
   await makeData()
 }
 if (process.argv[1].endsWith('/makedata.mjs')) {
